b79b81eb9e82243ca9083970cb836bd6930d3682
BlackSound1 <matthewr@videotron.ca>
2021-01-20 19:46:34 -0500

    added props. Notes now dyniamic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
     return (<div>
             <Header />
-            <Note className= "note" />
+
+            {notes.map(note => 
+                    <Note className="note" 
+                    key={note.key}
+                    title={note.title} 
+                    content={note.content} />
+                )
+            }
+
             <Footer />
         </div>
     );
